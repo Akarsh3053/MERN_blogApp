4b3578ff388b3e9e82379303d310a05f8cee4791
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-06-16 15:11:11 +0530

    fix updates

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Footer } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
+export default function FooterCom() {
+  return (
+    <Footer container className='border border-t-8 border-teal-500'>
+      <div className='w-full max-w-7xl mx-auto'>
+      </div>
+    </Footer>
+  );
+}
